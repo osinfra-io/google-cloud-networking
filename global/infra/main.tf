438e634c51f72771a8adf6e9f04da9376603f1f2
Brett Curtis <brett@osinfra.io>
2023-09-07 07:02:35 -0400

    Enable APIs required by the Datadog module

--- global/infra/main.tf
+++ global/infra/main.tf
@@ -60,6 +60,12 @@ module "project" {
   prefix = "plt-lz"
 
   services = [
+    "cloudasset.googleapis.com",
+    "cloudbilling.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+    "compute.googleapis.com",
+    "iam.googleapis.com",
+    "monitoring.googleapis.com",
     "dns.googleapis.com"
   ]
 }
